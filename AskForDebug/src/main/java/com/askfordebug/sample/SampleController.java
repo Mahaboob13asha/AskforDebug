c45393c61217e59ab1a5a48301917e023ea3a530
Mahaboob <Mahaboob@Admin-PC>
2018-04-25 11:09:35 +0530

    logger implementation....

--- AskForDebug/src/main/java/com/askfordebug/sample/SampleController.java
+++ AskForDebug/src/main/java/com/askfordebug/sample/SampleController.java
@@ -5,11 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
 
 @Controller
 @RequestMapping("VIEW")
 public class SampleController {
-	
+	private final static Log _log = LogFactoryUtil.getLog(SampleController.class);
 	@RenderMapping
 	public String question(Model model) {
 
